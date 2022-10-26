77e5dac5e17c3bc3d71dfaa1795de9da0c1293dd
Hector Felipee <rexnike12@gmail.comm>
2022-10-26 12:52:00 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()
+  );
+}
+
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
